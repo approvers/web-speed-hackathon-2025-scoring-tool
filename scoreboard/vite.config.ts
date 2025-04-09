d60c3f3f0e22de7ecb3dd1a6457204ed8fda2020
kawaemon <me@kawaemon.dev>
2025-04-09 10:51:24 +0900

    base

--- scoreboard/vite.config.ts
+++ scoreboard/vite.config.ts
@@ -4,6 +4,7 @@ import preact from "@preact/preset-vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/web-speed-hackathon-2025-scoring-tool",
   build: {
     minify: "terser",
     terserOptions: { compress: { passes: 5 } },
